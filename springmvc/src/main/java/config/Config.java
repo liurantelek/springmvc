2034e20f58b0a8ca24cb85004362a37a13005845
liuran <454176801@qq.com>
2019-12-20 08:38:52 +0800

    init

---
+++ springmvc/src/main/java/config/Config.java
@@ -0,0 +1,31 @@
+package config;
+/**
+ * @Auther: 45417
+ * @Date: 2019/12/17 18:32
+ * @Description:
+ */
+
+import lombok.Data;
+import org.omg.CORBA.CODESET_INCOMPATIBLE;
+
+/**
+ *
+ * @ProjectName: springmvc
+ * @ClassName: Config
+ * @Description: TODO(一句话描述该类的功能)
+ * @Author: 45417
+ * @Date: 2019/12/17 18:32
+ * @version v1.0
+ *
+ */
+@Data
+public class Config {
+
+    private String className;
+
+    private String beanName;
+
+    public static void main(String[] args){
+
+    }
+}
